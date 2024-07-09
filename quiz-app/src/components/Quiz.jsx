883c7b1f6c2ee6b63e367dc9f2557604299d2224
Abhiram Deshpande <designing.developer111@gmail.com>
2024-07-09 11:55:40 +0530

    added Question timer

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizComplete from '../assets/quiz-complete.png'
+import QuestionTimer from "./QuestionTimer";
 export default function Quiz() {
+
   const [userPickedAnswers, setUserPickedAnswers] = useState([]);
   let activeQuestionIndex = userPickedAnswers.length;
+ 
+
+  let isQuizOver = activeQuestionIndex == QUESTIONS.length
 
   function handleSelectAnswer(selectedAnswer) {
     setUserPickedAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   }
 
+  if (isQuizOver) {
+    return (<div id="summary">
+      <img src={quizComplete} />
+      <h2>Quiz Completed</h2>
+    </div>)
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  shuffledAnswers.sort(() => Math.random() - 0.5)
+
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={ () => handleSelectAnswer(null) }/>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
